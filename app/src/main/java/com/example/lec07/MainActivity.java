e10d38c9d95fe6f1c3c82ca90772acb7236d4162
saad <saadh9290@gmail.com>
2021-11-05 17:15:26 +0500

    linear Layout

--- app/src/main/java/com/example/lec07/MainActivity.java
+++ app/src/main/java/com/example/lec07/MainActivity.java
@@ -5,19 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     CheckBox ch;
     TextView tx;
+    TextView ab;
+    Switch btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ch=findViewById(R.id.checkBox);
-        tx=findViewById(R.id.textView);
-        ch.setOnClickListener(new View.OnClickListener() {
+       // ch=findViewById(R.id.checkBox);
+        //tx=findViewById(R.id.textView);
+        ab=findViewById(R.id.editTextTextPersonName2);
+        btn=(Switch)findViewById(R.id.switch2);
+        /*ch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(ch.isChecked())
@@ -27,6 +32,19 @@ public class MainActivity extends AppCompatActivity {
                 else
                 {
                     tx.setText("check button is disable");
+                }
+            }
+        });*/
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (btn.isChecked())
+                {
+                          ab.setText("switch button is enabled");
+                }
+                else
+                {
+                    ab.setText("switch button is dissabled");
                 }
             }
         });
